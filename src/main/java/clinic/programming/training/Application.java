a6820e6e69ff8d044d947e850ee19ba8f82cdfc4
Robert Moore <moore.robert.ryan@gmail.com>
2020-07-02 15:05:09 +0200

    Use StringUtils and added dependancy for lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,7 +2,14 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import  org.apache.commons.lang3.StringUtils;
+
 public class Application {
+
+    public int coutnWords(String words){
+        String[] separatWords = StringUtils.split(words, ' ');
+        return (separatWords == null) ? 0: separatWords.length;
+    }
 
     public void greet(){
         List<String> greetings = new ArrayList<>();
@@ -22,5 +29,7 @@ public class Application {
         System.out.println ("Starting Application");
         Application app = new Application();
         app.greet();
+        int count = app.coutnWords("I have four words");
+        System.out.println("Wor count: "+ count);
     }
 }
